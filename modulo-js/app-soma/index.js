14a9e8d6f9803d8fbcc221ee6184587c4cf184c7
mariorzk <mariorzk@gmail.com>
2025-04-16 19:05:05 -0300

    app de soma simples.

---
+++ modulo-js/app-soma/index.js
@@ -0,0 +1,10 @@
+let num1 = Number(prompt("Digite o Primeiro Número"));
+let num2 = Number(prompt("Digite o segundo número"));
+alert("Somando...");
+
+res = num1 + num2;
+
+alert(`O Resultado da soma de ${num1} + ${num2} é ${res}!`);
+
+document.writeln("<h3>Para realizar uma nova soma por favor recarregue a página!</h3>")
+
