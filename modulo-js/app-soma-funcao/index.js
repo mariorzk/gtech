7123e5715b99c146e85bc40a5b0861e8fecb1c40
mariorzk <mariorzk@gmail.com>
2025-04-17 22:42:34 -0300

    app de soma com função 2.0

--- modulo-js/app-soma-funcao/index.js
+++ modulo-js/app-soma-funcao/index.js
@@ -1,9 +1,7 @@
 
-function Soma(){
-    num1 = Number(prompt("Digite o primeiro número:"))
-    num2 = Number(prompt("Digite o segundo número:"))
-    res = num1 + num2
-    document.writeln(`O resultado da soma é ${res}`)
+function Soma(n1, n2){
+   return n1 + n2;
 }
-
+let res = Soma(Number(prompt("Digite o primeiro número:")), Number(prompt("Digite o segundo númeor:")));
+document.writeln(`O resultado da soma é ${res}`);
 Soma()
