75bb04fea06fdd53f7540928863f8dddb6047124
mariorzk <mariorzk@gmail.com>
2025-04-18 21:53:38 -0300

    localStorage, setTimeout e setInterval

---
+++ modulo-js/local storage e Timeout/index.js
@@ -0,0 +1,9 @@
+localStorage.setItem("nome", "Mario");
+
+// setTimeout(() => {
+//     alert("Olá pessoal!")
+// }, 3000);
+
+setInterval(() => {
+    alert("Olá pessoal, eu apareço a cada 3 segundos")
+}, 3000);
