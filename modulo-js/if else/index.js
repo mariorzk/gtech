231d40a68662b5b94c0853a1bbe5607523e0dcd6
mariorzk <mariorzk@gmail.com>
2025-04-18 21:59:04 -0300

    if e else

---
+++ modulo-js/if else/index.js
@@ -0,0 +1,9 @@
+num = Number(prompt("Digite um numero:"));
+
+if (num > 5){
+    alert("O número é maior que 5!!")
+} else if (num < 5) {
+    alert("O número é menor que 5!!")
+}else {
+    alert("O número é igual a 5!!")
+}
